c54a2c66de731ba4fe075a4d0133842fb33db86b
sbsyme <sbsyme@gmail.com>
2025-04-11 22:17:01 +0100

    feat(openai-integration): OpenAI assistant integration.  - Adding new server action to fetch meal plan from assistant given form input.  - Testing environment setup to read meal plan from static json.

--- src/app/requestMealPlan/page.tsx
+++ src/app/requestMealPlan/page.tsx
@@ -1,20 +1,21 @@
 'use client';
 
 import Form from 'next/form'
-import {postMealPlan} from "@/app/actions";
 import {MealPlan} from "@/types/types";
 import SubmitButton from "@/components/SubmitButton";
 import SupermarketInput from "@/components/SupermarketInput";
 import BudgetInput from "@/components/BudgetInput";
 import React from "react";
 import MealPlanSummary from "@/components/MealPlanSummary";
+import {fetchMealPlan} from "@/lib/openai";
 
 export default function RequestMealPlanForm() {
+
     const [mealPlanData, setMealPlanData] = React.useState<MealPlan>()
     const [fetchedMealPlan, setFetchedMealPlan] = React.useState(false)
 
     function createMealPlan(formData: FormData) {
-        postMealPlan(formData)
+        fetchMealPlan(formData)
             .then(result => {
                 setMealPlanData(result)
                 setFetchedMealPlan(true)
--- src/components/MealPlanSummary.tsx
+++ src/components/MealPlanSummary.tsx
@@ -1,16 +1,21 @@
 'use client';
 
 import {MealPlan} from "@/types/types";
+import React from 'react'
 
-export default function MealPlanSummary({mealPlan} : {mealPlan : MealPlan}) {
+export default function MealPlanSummary({mealPlan}: { mealPlan: MealPlan }) {
 
-    console.log(mealPlan.meal_plan);
-    console.log(mealPlan);
-    
     return (<div className="p-10">
-            Fetched meal plan
-            <br/>
-            {mealPlan.shopping_list.map(item => item.name)}
+            <h1>Shopping List</h1>
+            <ul>
+                {mealPlan.shopping_list.map((item) => (
+                    <li key={item.name}>
+                        <h2>{item.price.amount}</h2>
+                        <h3>{item.quantity}</h3>
+                        <h4>{item.pricing_ref}</h4>
+                    </li>
+                ))}
+            </ul>
         </div>
     );
 };
