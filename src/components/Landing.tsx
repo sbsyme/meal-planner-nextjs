bfbea3903ba02d341f432f92ffe7d2355489d920
sbsyme <sbsyme@gmail.com>
2025-05-04 15:33:14 +0100

    Fixing build issues and typos.

--- src/components/Landing.tsx
+++ src/components/Landing.tsx
@@ -11,10 +11,10 @@ export default function Landing() {
         <section>
             <div className="mx-auto max-w-screen-xl text-center">
                 <h1 className="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-white">
-                    Automatically build a meal plan customised to your needs.</h1>
+                    Automatically build a meal plan tailored to your needs.</h1>
                 <p className="mb-8 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 lg:px-48 dark:text-gray-400">
-                    Using AI, this tool generates a full weekly meal plan which includes shopping list and
-                    recipes, based on your budget and preferred supermarket
+                    Using AI, this tool generates a weekly meal plan which includes a shopping list and
+                    recipes, based on your budget and preferred supermarket.
                 </p>
                 {!user ?
                     <GenericButton text="Get Started" href="/auth/login?returnTo=/getting-started"/>
